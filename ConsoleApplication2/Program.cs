cdf42e42c092ee0a71d8a0c59d637b1ad0893271
Besttdk <Besttdk@ASSELBY>
2017-08-30 01:13:56 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Net;
+
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebClient client = new WebClient())
+            {
+                
+                string url = "https://mail.ru";
+                string wrongUrl = "https://mai1.ru";
+                try { 
+               // string content = client.DownloadString(url);
+                string secondContent = client.DownloadString(wrongUrl);
+               // Console.WriteLine(content);//верный
+                Console.WriteLine(secondContent);//не верный;
+                }
+                catch (System.Net.WebException e  )
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ConsoleApplication2/WrongWebPageException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ConsoleApplication2
+{
+   public  class WrongWebPageException:WebException
+    {
+        public override string Message
+        {
+           get
+            {
+                return string.Format("Wrong web page!");
+            }
+        }
+    }
+}
